a81a90b6046ccf08aa54deb9fb1a2ed17525e1f9
Ali Nazari <ali.nazari@aut.ac.ir>
2019-09-09 15:48:11 +0430

    refactor files

--- Quera/badkhah_pooyan.py
+++
@@ -1,7 +0,0 @@
-p, d = map(int, input().split())
-while True:
-    if d % p <= p / 2:
-        print(d)
-        break
-    else:
-        d += d
--- Quera/yek_soal_sadeh.py
+++
@@ -1,3 +0,0 @@
-number = int(input())
-for i in range(number):
-    print("man khoshghlab hastam")
